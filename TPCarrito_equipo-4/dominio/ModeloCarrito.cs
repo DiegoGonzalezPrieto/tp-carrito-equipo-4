b93356ec4688d396e670579ff8346586d6a02344
Diego Prieto <diego_prieto@itrsa.com.ar>
2024-05-10 15:22:24 -0300

    agrega modelos: Carrito e ItemCarrito

---
+++ TPCarrito_equipo-4/dominio/ModeloCarrito.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class ModeloCarrito
+    {
+        public List<ItemCarrito> Items { get; set; } = new List<ItemCarrito>();
+
+        public decimal ImporteTotal
+        {
+            get { return Items.Sum(i => i.SubTotal); }
+        }
+
+        public void agregarItem(Articulo articulo)
+        {
+            foreach (ItemCarrito item in Items)
+            {
+                if (item.Id == articulo.Id)
+                {
+                    item.Cantidad++;
+                    return;
+                }
+            }
+            Items.Add(new ItemCarrito(articulo));
+            return;
+        }
+        public void quitarItem(Articulo articulo)
+        {
+            foreach (ItemCarrito item in Items)
+            {
+                if (item.Id == articulo.Id)
+                {
+                    if (item.Cantidad > 1)
+                        item.Cantidad--;
+                    else
+                        Items.Remove(item);
+                    return;
+                }
+            }
+            throw new Exception("El item no se encuentra en el carrito.");
+        }
+    }
+}
